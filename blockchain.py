81acd3ec4d2437664dd58ed7a2a48b522bd84c0c
ogken03 <kennyle43@gmail.com>
2024-09-21 19:13:08 -0700

    Added more arguments to parse commands

--- blockchain.py
+++ blockchain.py
@@ -1,79 +1,127 @@
 import argparse
 
 
-# bchoc add -c case_id -i item_id [-i item_id ...] -g creator -p password(creator’s)
-# bchoc checkout -i item_id -p password
-# bchoc checkin -i item_id -p password
-# bchoc show cases 
-# bchoc show items -c case_id
-# bchoc show history [-c case_id] [-i item_id] [-n num_entries] [-r] -p password
-# bchoc remove -i item_id -y reason -p password(creator’s)
-# bchoc init
-# bchoc verify
-
 class Blockchain:
     def __init__(self):
         self.chain = []
-    #add a part to load up a json or csv file
+    # Add a part to load up a JSON or CSV file
+
 
 class Block:
-
     def __init__(self) -> None:
         pass
 
 
+def add(args):
+    print(f"Adding item(s)")
+
+
+def checkout_item(args):
+    print(f"Checking out item")
+
+
+def checkin_item(args):
+    print(f"Checking in item")
+
+
+def show_cases(args):
+    print("Showing all cases")
+
+
+def show_items(args):
+    print(f"Showing items")
+
+
+def show_history(args):
+    print(f"Showing history")
+
+
+def remove(args):
+    print(f"Removing item")
+
+
+def init(args):
+    print("Initializing blockchain")
+
+
+def verify(args):
+    print("Verifying blockchain")
+
+
 def main():
-    
-    #Setup parsers
-    mainParser = argparse.ArgumentParser()
-    secondParser = mainParser.add_subparsers(dest='choices')
+    try:
+        #Parsing TIME
+        mainParser = argparse.ArgumentParser()
+        subparsers = mainParser.add_subparsers(dest='choices')
 
-    #Adding
-    addBlock = secondParser.add_parser('add')
-    addBlock.add_argument('-c', '--case_id', required=True)
-    addBlock.add_argument('-i', '--item_ids', nargs='+', required=True)
-    addBlock.add_argument('-g', '--creator', required=True)
-    addBlock.add_argument('-p', '--password', required=True)
-    #Checkout
-    checkoutBlock = secondParser.add_parser('checkout')
-    #Checkin
-    checkinBlock = secondParser.add_parser('checkin')
-    #Showing
-    showCasesBlock = secondParser.add_parser('show cases')
+        #Adding Blocks
+        addBlock = subparsers.add_parser('add')
+        addBlock.add_argument('-c', '--case_id', required=True)
+        addBlock.add_argument('-i', '--item_id', nargs='+', required=True)
+        addBlock.add_argument('-g', '--creator', required=True)
+        addBlock.add_argument('-p', '--password', required=True)
+        addBlock.set_defaults(func=add)
 
-    showItemsBlock = secondParser.add_parser('show items')
+        #Checkout blocks
+        checkoutBlock = subparsers.add_parser('checkout')
+        checkoutBlock.add_argument('-i', '--item_id', required=True)
+        checkoutBlock.add_argument('-p', '--password', required=True)
+        checkoutBlock.set_defaults(func=checkout_item)
 
-    showHistoryBlock = secondParser.add_parser('show history')
+        #Checkin Blocks
+        checkinBlock = subparsers.add_parser('checkin')
+        checkinBlock.add_argument('-i', '--item_id', required=True)
+        checkinBlock.add_argument('-p', '--password', required=True)
+        checkinBlock.set_defaults(func=checkin_item)
 
-    #Remove
-    removeBlock = secondParser.add_parser('remove')
-    #Init
-    initBlock = secondParser.add_parser('init')
-    #Verify
-    verifyBlock = secondParser.add_parser('verify')
+        #Showing subparser (because argparse doesnt do whitespace ugh)
+        showBlock = subparsers.add_parser("show")
+        showSubparser = showBlock.add_subparsers(dest='showing')
+        #Showing Cases 
+        showCasesBlock = showSubparser.add_parser('cases')
+        showCasesBlock.set_defaults(func=show_cases)
 
-    #Grab Commands
-    args = mainParser.parse_args()
+        #Showing Items
+        showItemsBlock = showSubparser.add_parser('items')
+        showItemsBlock.add_argument('-c', '--case_id', required=True)
+        showItemsBlock.set_defaults(func=show_items)
 
-    #Call functions
-    if args.choices == 'add':
-        print('add')
-    if args.choices == 'checkout':
-        print('checkout')
-    if args.choices == 'checkin':
-        print('checkin')
-    if args.choices == 'show cases':
-        print('show cases')
-    if args.choices == 'show items':
-        print('show items')
-    if args.choices == 'show history':
-        print('show history')
-    if args.choices == 'remove':
-        print('remove')
-    if args.choices == 'init':
-        print('init')
-    if args.choices == 'add':
-        print('verify')
+        #showting history
+        showHistoryBlock = showSubparser.add_parser('history')
+        showHistoryBlock.add_argument('-c', '--case_id')
+        showHistoryBlock.add_argument('-i', '--item_id')
+        showHistoryBlock.add_argument('-n', '--num_entries')
+        showHistoryBlock.add_argument('-r', '--reverse')
+        showHistoryBlock.add_argument('-p', '--password', required=True)
+        showHistoryBlock.set_defaults(func=show_history)
+        #show block end
+
+        #Removing Blocks
+        removeBlock = subparsers.add_parser('remove')
+        removeBlock.add_argument('-i', '--item_id', required=True)
+        removeBlock.add_argument('-y', '--reason', required=True)
+        removeBlock.add_argument('-p', '--password', required=True)
+        removeBlock.set_defaults(func=remove)
+
+        #Init blocks
+        initBlock = subparsers.add_parser('init')
+        initBlock.set_defaults(func=init)
+
+        #Verifying 
+        verifyBlock = subparsers.add_parser('verify')
+        verifyBlock.set_defaults(func=verify)
+
+        # Parse arguments
+        args = mainParser.parse_args()
+
+        if hasattr(args, 'func'):
+            args.func(args)
+        else:
+            print("function calls are wrong or something idk.")
+
+    except Exception as error:
+        print(f"An error occurred: {error}")
+
 
 if __name__ == "__main__":
     MainBlockchain = Blockchain()
