de2b5bc1b0e2a9695d7b5ffffb593fa0cf621cd1
Darren Deng <dxdeng@asu.edu>
2024-09-29 17:04:40 -0700

    Added simple encryption/decryption functions

--- cryptography.py
+++ cryptography.py
@@ -1,5 +1,21 @@
-# TODO: Handle decryption/encryption here
-
-from Crypto.Cipher import _AES
+from Crypto.Cipher import AES
 import os
-import hashlib
+
+DEFAULT_KEY = b"R0chLi4uLi4uLi4="
+AES_KEY = os.environ.get("AES_KEY", DEFAULT_KEY)
+
+def encrypt(data):
+    """
+    Encrypts data
+    """
+    cipher = AES.new(AES_KEY, AES.MODE_ECB)
+    paddedData = data.encode('utf-8').ljust(32, b'\0') # ljust (left adjust) to adjust for padding
+    return cipher.encrypt(paddedData)
+
+def decrypt(data):
+    """
+    Decrypts data
+    """
+    cipher = AES.new(AES_KEY, AES.MODE_ECB)
+    decryptedData = cipher.decrypt(data)
+    return decryptedData.rstrip(b'\0').decode('utf-8')
