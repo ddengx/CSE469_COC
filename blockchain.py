98e938e95022fe96ebb2e9b367fb31a332a96b72
ddengx <100387783+ddengx@users.noreply.github.com>
2024-10-01 03:44:16 -0700

    Added simple blockchain methods

--- blockchain.py
+++ blockchain.py
@@ -1,31 +1,57 @@
-
-# TODO: Implement blockchain class here
-
-# Required block struct below
-#INITIAL BLOCK =
-#Prev_hash = 0, # 32 bytes 
-#Timestamp = 0, # 08 bytes
-#Case_id = b"0"*32, # 32 bytes (32 zero's) 
-#Evidence_id = b"0"*32, # 32 bytes (32 zero's) 
-#State = b"INITIAL\0\0\0\0\0", # 12 bytes
-#creator = b"\0"*12, # 12 bytes (12 null bytes) 
-#owner = b"\0"*12, # 12 bytes (12 null bytes) 
-#D_length = 14, # 04 bytes
-#Data = b"Initial block\0"
-
 import os
+import struct
 from block import Block
-import argparse
-
 
 class Blockchain:
     def __init__(self):
+        self.blockchainFilePath = os.getenv("BCHOC_FILE_PATH", "blockchain.dat")
         self.chain = []
         self.load_blockchain()
 
-    # Import blockchain data
-    # Nab from environ
-    # Add checks for absence of, or lack thereof, blocks here
     def load_blockchain(self):
-        pass
-    # Add a part to load up a JSON or CSV file
+        if not os.path.exists(self.blockchainFilePath):
+            print("Blockchain file not found. Created INITIAL block.")
+            self.create_initial_block()
+        else:
+            with open(self.blockchainFilePath, 'rb') as file:
+                fileSize = os.path.getsize(self.blockchainFilePath)
+                if fileSize == 0:
+                    print("Blockchain file is empty. Creating INITIAL block.")
+                    self.create_initial_block()
+                else:
+                    print("Loading blockchain data...")
+                    self.read_blocks(file)
+
+    def read_blocks(self, file):
+        blockHeaderSize = struct.calcsize("32s d 32s 32s 12s 12s 12s I")
+
+        while True:
+            blockHeader = file.read(blockHeaderSize)
+
+            if not blockHeader:
+                print("Reading a file with NO BLOCK IDITO")
+                break
+
+            prev_hash, timestamp, case_id, evidence_id, state, creator, owner, D_length = struct.unpack(
+                "32s d 32s 32s 12s 12s 12s I", blockHeader
+            )
+
+            dataField = file.read(D_length)
+            blockBinaryData = blockHeader + dataField
+            block = Block.from_binary(blockBinaryData)
+            self.chain.append(block)
+
+    def create_initial_block(self):
+        initialBlock = Block(
+            prevHash = "0",
+            timestamp = 0,
+            caseID = "00000000-0000-0000-0000-000000000000",
+            evidenceID = 0,
+            state = "INITIAL",
+            creator = "",
+            owner = "",
+            data = "Initial block"
+        )
+        with open(self.blockchainFilePath, 'wb') as file:
+            file.write(initialBlock.to_binary())
+        self.chain.append(initialBlock)
