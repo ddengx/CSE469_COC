c122d01249e870a6de2ced5a84e7cfdff42edbda
ddengx <100387783+ddengx@users.noreply.github.com>
2024-10-05 03:20:26 -0700

    Created append chain function

--- blockchain.py
+++ blockchain.py
@@ -4,11 +4,18 @@ from block import Block
 
 class Blockchain:
     def __init__(self):
+        """
+        Initialize an instance (always concurrent never unique) of the blockchain
+        """
         self.blockchainFilePath = os.getenv("BCHOC_FILE_PATH", "blockchain.dat")
         self.chain = []
         self.load_blockchain()
 
     def load_blockchain(self):
+        """
+        Loads the blockchain file
+        Creates an initial block if needed
+        """
         if not os.path.exists(self.blockchainFilePath):
             print("Blockchain file not found. Created INITIAL block.")
             self.create_initial_block()
@@ -22,9 +29,13 @@ class Blockchain:
                     self.read_blocks(file)
 
     def read_blocks(self, file):
+        """
+        Read the blockchain file (from binary) and appends it to the list (does not write)
+        """
+        # Size of a block - without the data field
         blockHeaderSize = struct.calcsize("32s d 32s 32s 12s 12s 12s I")
         while True:
-            blockHeader = file.read(blockHeaderSize)
+            blockHeader = file.read(blockHeaderSize) # Read by size
 
             if not blockHeader:
                 break
@@ -39,6 +50,9 @@ class Blockchain:
             self.chain.append(block)
 
     def create_initial_block(self):
+        """
+        Defines the structure of an initial block and appends it to the chain
+        """
         initialBlock = Block(
             prevHash = b"\0"*32, # We should store the previous as hashes (see block constructor comment)
             timestamp = 0,
@@ -49,6 +63,12 @@ class Blockchain:
             owner = "",
             data = "Initial block"
         )
-        with open(self.blockchainFilePath, 'wb') as file:
-            file.write(initialBlock.to_binary())
-        self.chain.append(initialBlock)
+        self.append_chain(initialBlock)
+
+    def append_chain(self, newBlock):
+        """
+        Appends the block to the chain AND writes it to the file
+        """
+        self.chain.append(newBlock)
+        with open(self.blockchainFilePath, 'ab') as file:
+            file.write(newBlock.to_binary())
